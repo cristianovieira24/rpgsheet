fb1570c60f112b67cd014a6e381d87c6982709d4
cris <cristiano.silva.vieira24@gmail.com>
2026-08-11 19:14:54 -0300

    Update vite.github.config.ts

--- arcana-ficha-5e-github/vite.github.config.ts
+++ arcana-ficha-5e-github/vite.github.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ficha-d-d/",
+  base: "/rpgsheet/",
   plugins: [react()],
   build: {
     outDir: "gh-pages-dist",
